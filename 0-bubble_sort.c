abe4724789b1303cb489b36e5384ad16b8a07963
Alex Guadalupe <9035@holbertonstudents.com>
2024-04-09 18:55:43 -0400

    task 0 fix betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,22 +8,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, p, temp = 0;
+size_t i, p, temp = 0;
 
-	if (size < 2)
-		return;
+if (size < 2)
+return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		for (p = 0; p < size - 1; p++)
-		{
-			if (array[p] > array[p + 1])
-			{
-				temp = array[p + 1];
-				array[p + 1] = array[p];
-				array[p] = temp;
-				print_array(array, size);
-			}
-		}
-	}
+for (i = 0; i < size - 1; i++)
+{
+for (p = 0; p < size - 1; p++)
+{
+if (array[p] > array[p + 1])
+{
+temp = array[p + 1];
+array[p + 1] = array[p];
+array[p] = temp;
+print_array(array, size);
+}
+}
+}
 }
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in
+ * ascending order.
+ * @list: list
+ *
+ * Return: void
+*/
+void insertion_sort_list(listint_t **list)
+{
+listint_t *current, *swap, *tmp;
+
+if (list == NULL || *list == NULL)
+return;
+current = *list;
+while ((current = current->next))
+{
+swap = current;
+while (swap->prev && swap->n < swap->prev->n)
+{
+tmp = swap->prev;
+if (swap->next)
+swap->next->prev = tmp;
+if (tmp->prev)
+tmp->prev->next = swap;
+else
+*list = swap;
+tmp->next = swap->next;
+swap->prev = tmp->prev;
+swap->next = tmp;
+tmp->prev = swap;
+print_list(*list);
+}
+}
+}
