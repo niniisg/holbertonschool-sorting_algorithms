637a52751a90c75a9eea9efc36ffd66c08f58cb2
niniisg <nisselsanchez@gmail.com>
2024-04-10 18:41:28 -0400

    sort.h

--- sort.h
+++ sort.h
@@ -12,17 +12,10 @@
  */
 typedef struct listint_s
 {
-<<<<<<< HEAD
+
 const int n;
 struct listint_s *prev;
 struct listint_s *next;
-=======
- const int n;
- struct listint_s *prev;
- struct listint_s *next;
->>>>>>> main
-} listint_t;
-
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -30,11 +23,5 @@ void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 <<<<<<< HEAD
 void print_list(const listint_t *list);
-=======
-<<<<<<< HEAD
-=======
-void print_list(const listint_t *list);
 
->>>>>>> nissel
->>>>>>> main
 #endif
