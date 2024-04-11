44e4f77774d615a8280c45f656d31d29f4e0caf8
niniisg <nisselsanchez@gmail.com>
2024-04-10 20:59:03 -0400

    sort.h

--- sort.h
+++ sort.h
@@ -21,7 +21,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-<<<<<<< HEAD
 void print_list(const listint_t *list);
 
 #endif
